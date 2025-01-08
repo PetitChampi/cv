924c7917a1d6c294170013870bea9ace735f033c
Esther Haddon <48768205+PetitChampi@users.noreply.github.com>
2025-01-08 15:39:18 +0100

    edits Jan 2025, mostly english

---
+++ script.js
@@ -0,0 +1,4 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+const yearsSince2018 = currentYear - 2018;
+document.getElementById("yearsSince2018").textContent = yearsSince2018;
